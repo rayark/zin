9f828a83a2ae1d6af6a963130b66c7bf3e9e4326
Yao Wei <mwei@rayark.com>
2016-05-20 15:54:15 +0800

    feat: pass age for cache control header

--- middleware/cachecontrol.go
+++ middleware/cachecontrol.go
@@ -1,13 +1,17 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
-func CacheControl(h httprouter.Handle) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		w.Header().Add("Cache-Control", "public, s-maxage=86400")
-		h(w, r, p)
+func CacheControl(age int) func(h httprouter.Handle) httprouter.Handle {
+	value := fmt.Sprintf("public, s-maxage=%s", age)
+	return func(h httprouter.Handle) httprouter.Handle {
+		return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+			w.Header().Add("Cache-Control", value)
+			h(w, r, p)
+		}
 	}
 }
