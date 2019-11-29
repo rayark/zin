a1308901a4c9cb758f6e12cd0d04b392a1ba5d30
mswu <mswu@rayark.com>
2019-11-29 14:21:17 +0800

    Fix using `%s` to print out integer in CacheControl

--- middleware/cachecontrol.go
+++ middleware/cachecontrol.go
@@ -8,7 +8,7 @@ import (
 )
 
 func CacheControl(age int) func(h httprouter.Handle) httprouter.Handle {
-	value := fmt.Sprintf("public, s-maxage=%s", age)
+	value := fmt.Sprintf("public, s-maxage=%d", age)
 	return func(h httprouter.Handle) httprouter.Handle {
 		return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 			w.Header().Add("Cache-Control", value)
