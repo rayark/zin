45affdbc9ec6c2ebe16a0e233c9322a8db0019fa
賈仁宇 <raymondchia@rayark.com>
2023-01-12 18:13:33 +0800

    Add copyright

--- middleware/recoverer.go
+++ middleware/recoverer.go
@@ -1,3 +1,14 @@
+/* (C)2023 Rayark Inc. - All Rights Reserved
+ * Rayark Confidential
+ *
+ * NOTICE: The intellectual and technical concepts contained herein are
+ * proprietary to or under control of Rayark Inc. and its affiliates.
+ * The information herein may be covered by patents, patents in process,
+ * and are protected by trade secret or copyright law.
+ * You may not disseminate this information or reproduce this material
+ * unless otherwise prior agreed by Rayark Inc. in writing.
+ */
+
 package middleware
 
 import (
--- middleware/terminal.go
+++ middleware/terminal.go
@@ -1,3 +1,14 @@
+/* (C)2023 Rayark Inc. - All Rights Reserved
+ * Rayark Confidential
+ *
+ * NOTICE: The intellectual and technical concepts contained herein are
+ * proprietary to or under control of Rayark Inc. and its affiliates.
+ * The information herein may be covered by patents, patents in process,
+ * and are protected by trade secret or copyright law.
+ * You may not disseminate this information or reproduce this material
+ * unless otherwise prior agreed by Rayark Inc. in writing.
+ */
+
 package middleware
 
 // copy from https://github.com/zenazn/goji/blob/46b46e00f46dda650e04f0bceeb7c40732cd5988/web/middleware/terminal.go
